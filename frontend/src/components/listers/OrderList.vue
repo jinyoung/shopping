069bc260530bbbbef8a286013408c2c968c5e006
Jinyoung Jang <jyjang@uengine.org>
2022-08-02 17:02:19 +0900

    commit msg

--- frontend/src/components/listers/OrderList.vue
+++ frontend/src/components/listers/OrderList.vue
@@ -13,6 +13,7 @@
                             
                             
                             
+                            
                         </v-list-item-title>
 
                         <v-list-item-subtitle style="font-size:25px; font-weight:700;">
@@ -20,6 +21,7 @@
                             [ ProductId :  {{data.productId }} ] &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             [ Qty :  {{data.qty }} ] &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             [ Address :  {{data.address }} ] &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            [ Status :  {{data.status }} ] &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </v-list-item-subtitle>
 
                     </v-list-item-content>
@@ -104,6 +106,7 @@
                 'productId': '',
                 'qty': 0,
                 'address': '',
+                'status': '',
             }
         },
         methods: {
